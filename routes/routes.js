228597b00f494ebac770027f90a005a0a07da7d2
Habib Al Hasan Himel <djstar.himel@gmail.com>
2021-12-12 22:41:54 +0600

    root route added

---
+++ routes/routes.js
@@ -0,0 +1,25 @@
+
+const route = [
+
+    {
+        path:'/',
+        controller:(req,res)=>{
+
+            res.redirect('/explore')
+           
+        }
+    }
+
+
+]
+
+module.exports = (app)=>{
+    route.forEach((r)=>{
+       if(r.path=='/'){
+           app.get(r.path,r.controller)
+       }
+       else{
+        app.use(r.path,r.controller)
+       }
+    })
+}
