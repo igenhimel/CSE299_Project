569c238a93758fc897c05593224388b62cc66586
Habib Al Hasan Himel <djstar.himel@gmail.com>
2021-12-14 02:48:45 +0600

    authentication related route added

---
+++ routes/authRoute.js
@@ -0,0 +1,39 @@
+
+const authRoute = require('express').Router()
+const {
+
+    SignUpGetController,
+    SignUpPostController,
+    LoginGetController,
+    LoginPostController,
+    LogoutController,
+    myData,
+    GooglePostController
+
+} = require('../controllers/authContoller')
+
+const passport =require('passport')
+
+
+const signupValidator = require('../validators/SignUpValidator')
+const loginvalidator = require('../validators/loginvalidator')
+const {isUnAuthenticated} = require('../middleware/authMiddleware')
+
+
+authRoute.get('/google',passport.authenticate('google',{scope:['profile','email']}))
+authRoute.get('/google/callback',passport.authenticate('google',{failureRedirect:'/'}),GooglePostController)
+
+authRoute.post('/mydata',myData)
+
+authRoute.get('/signup',isUnAuthenticated,SignUpGetController)
+authRoute.post('/signup',isUnAuthenticated,signupValidator,SignUpPostController)
+
+
+authRoute.get('/login',isUnAuthenticated,LoginGetController)
+authRoute.post('/login',isUnAuthenticated,loginvalidator,LoginPostController)
+
+
+authRoute.get('/logout',LogoutController)
+
+
+module.exports=authRoute
